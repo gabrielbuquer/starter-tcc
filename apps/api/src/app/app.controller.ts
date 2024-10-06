11d4d785f0c3ad6ca7002406812d3aa613e350ab
Gabriel Buquer <gabrielbuquerb@gmail.com>
2024-10-05 21:03:51 -0300

    feat: nest api

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -6,7 +6,19 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // @Get('/')
+  // @Render('index')
+  // home() {
+  //   return {};
+  // }
+
+  // @Get('/teste')
+  // @Render('teste')
+  // teste() {
+  //   return {};
+  // }
+
+  @Get('/api')
   getData() {
     return this.appService.getData();
   }
