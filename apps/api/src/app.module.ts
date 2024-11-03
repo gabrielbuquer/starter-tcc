a68585ac7494bfffb98ce2eb57e2698f82b80576
marcos.ant.pereira.jr <marcos.ant.pereira.jr@gmail.com>
2024-11-03 20:38:51 -0300

    adding docker and user

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app/app.controller';
 import { AppService } from './app/app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    password: 'postgres',
+    username: 'postgres',
+    entities: [],
+    database: 'monetix',
+    synchronize: true,
+    logging: true,
+  }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
